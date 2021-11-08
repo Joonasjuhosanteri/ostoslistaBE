70707fed7e827fb8240cd44de61ec7e28797a0fd
Joonasjuhosanteri <81249550+Joonasjuhosanteri@users.noreply.github.com>
2021-11-08 21:56:15 +0200

    backendin palautus

---
+++ inc/headers.php
@@ -0,0 +1,13 @@
+<?php
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+}
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Accept, Content-Type, Access-Control-Allow-Header');
+header('Access-Control-Max-Age: 3600');
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    return 0;
+}
